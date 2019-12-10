6c8f8b21a2b65a9f3b6c2b3a56c5b1d34919358c
Anthony Anonde <demeyortj@gmail.com>
2019-12-10 12:05:01 +0100

    learining list

---
+++ learningList.py
@@ -0,0 +1,10 @@
+#1) remove white spaces in the following list
+#2) remove duplicate letters in the following list
+# ['r', 'e', 'm', 'o', 'v', 'e', ' ', 'd', 'u', 'p', 'l', 'i', 'c', 'a', 't', 'e', 's', ' ', 'i', 'n', ' ', 't', 'h', 'e', ' ', 'f', 'o', 'l', 'l', 'o', 'w', 'i', 'n', 'g', ' ', 'l', 'i', 's', 't']
+
+# 2 ans == ['u', 'a', 'o', 'i', 'r', 'm', 'f', ' ', 'h', 'l', 's', 'g', 'v', 'p', 'n', 'c', 't', 'd', 'e', 'w']
+words = "remove duplicates in the following list"
+list1 = list(words)
+    
+if __name__ == "__main__":
+  print(list1)
